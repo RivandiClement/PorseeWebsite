4be548e60687a661a0b437fc95f8980fc287d721
Rivandi Clement <rivandi.clement.cr@gmail.com>
2022-09-30 13:27:02 +0700

    fixing navbar scroll, button background color

--- header/index.js
+++ header/index.js
@@ -1,17 +1,17 @@
-import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 import Image from 'next/image'
+import {Link} from 'react-scroll'
 
 export default function Header() {
     return (
         <header className={styles.topnav}>
-                <div className={styles.logo}>
-                    <Image src="/PorseeWord.svg" width={183} height={46}></Image>
-                </div>
-                <Link href="#kitchen"><a className={styles.a}>Contact Us</a></Link>
-                <Link href="/Service"><a className={styles.a}>Service</a></Link>
-                <Link href="/"><a className={styles.a}>About</a></Link>
-                <Link href="/"><a className={styles.a}>Home</a></Link>
+            <Link href="/"><div className={styles.logo}>
+                <Image src="/PorseeWord.svg" id={styles.logoImg} width={183} height={46}></Image>
+            </div></Link>
+            <Link className={styles.menu} to={styles.fifthbox} smooth={true} offset={400} duration={400}>Contact Us</Link>
+            <Link className={styles.menu} to={styles.fourthbox} smooth={true} duration={400}>Service</Link>
+            <Link className={styles.menu} to={styles.thirdbox} smooth={true} duration={400}>About</Link>
+            <Link className={styles.menu} to="/">Home</Link>
         </header>
     );
 }
